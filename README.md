427090d8bdc6f1c8b8e343e3460580136cf41eb0
Hector Pulido <Hectorandrespp@gmail.com>
2017-12-26 23:05:28 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unity Rewind Mechanic
+Simple rewind mechanic made with Unity coded in C#, perfect for games like braid or Prince of persia
+
+This tutorial was made for Hector Pulido for his youtube channel https://www.youtube.com/channel/UCS_iMeH0P0nsIDPvBaJckOw
+
+Please consider Support on Patreon https://www.patreon.com/HectorPulido
---
+++ TimeRewind/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 1;
+    public float JumpSpeed = 5;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	void Update ()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, rb.velocity.z);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += Vector3.up * JumpSpeed;
+        }
+	}
+}
